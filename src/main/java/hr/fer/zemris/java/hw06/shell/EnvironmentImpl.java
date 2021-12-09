ed639cd41ae57a78cc36cfb8ee56a51c986c2602
Leo Goršić <gorsicleo@gmail.com>
2021-12-09 22:13:09 +0100

    Created EnvironmentImpl class

---
+++ src/main/java/hr/fer/zemris/java/hw06/shell/EnvironmentImpl.java
@@ -0,0 +1,73 @@
+package hr.fer.zemris.java.hw06.shell;
+
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class EnvironmentImpl implements Environment {
+	
+	private char multiLineSymbol = '|';
+	private char promptSymbol = '>';
+	private char morelinesSymbol = '\\';
+
+	@Override
+	public String readLine() throws ShellIOException {
+		Scanner sc = new Scanner(System.in);
+		if (sc.hasNextLine()) {
+			String nextLine = sc.nextLine();
+			sc.close();
+			return nextLine;
+		} else {
+			sc.close();
+			throw new ShellIOException("Unable to read next line!");
+		}
+	}
+
+	@Override
+	public void write(String text) throws ShellIOException {
+		System.out.print(text);
+
+	}
+
+	@Override
+	public void writeln(String text) throws ShellIOException {
+		System.out.println(text);
+
+	}
+
+	@Override
+	public SortedMap<String, ShellCommand> commands() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Character getMultilineSymbol() {
+		return multiLineSymbol;
+	}
+
+	@Override
+	public void setMultilineSymbol(Character symbol) {
+		multiLineSymbol = symbol;
+	}
+
+	@Override
+	public Character getPromptSymbol() {
+		return promptSymbol;
+	}
+
+	@Override
+	public void setPromptSymbol(Character symbol) {
+		promptSymbol = symbol;
+	}
+
+	@Override
+	public Character getMorelinesSymbol() {
+		return morelinesSymbol;
+	}
+
+	@Override
+	public void setMorelinesSymbol(Character symbol) {
+		morelinesSymbol = symbol;
+	}
+
+}
--- src/main/java/hr/fer/zemris/java/hw06/shell/ShellIOException.java
+++ src/main/java/hr/fer/zemris/java/hw06/shell/ShellIOException.java
@@ -1,5 +1,11 @@
 package hr.fer.zemris.java.hw06.shell;
 
-public class ShellIOException extends Exception {
+@SuppressWarnings("serial")
+public class ShellIOException extends RuntimeException {
 
+	public ShellIOException(String message) {
+		super(message);
+	}
+
+	
 }
